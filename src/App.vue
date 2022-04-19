2abe3b199d9f09bbcda78ec354b652dec2d6b79b
robbastos <roberto.bastos@prolins.com.br>
2022-04-19 14:53:47 -0300

    cria paginas e adiciona routers

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,6 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
---
+++ src/config/constants.js
@@ -0,0 +1,3 @@
+export default class Constants {
+    static BASE_URL = 'https://private-5b8666-testefrontendpc4.apiary-mock.com/users';
+}
